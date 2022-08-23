9112669477f091889a18e62d3ca5aa6aceab2d82
Chethanck07 <chethankumar.k01@infosys.com>
2022-08-23 14:49:18 +0530

    commiting to local repository

---
+++ src/First_Project/NewClass.java
@@ -0,0 +1,23 @@
+package First_Project;
+
+public class NewClass {
+	
+		
+		public static void main(String[] args) {
+			System.out.println("enter two numbers:");
+			int first =20;
+			int second =20;
+			
+			System.out.println(first +" "+ second);
+			
+			int sum  = first+second;
+			System.out.println("the sum is: "+ sum);
+			
+			
+			
+		}
+
+	}
+
+
+
